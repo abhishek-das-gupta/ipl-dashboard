e29fe696d9fb9b99c6d01f78bbcf482c8d61e062
onTheQT <abhishekdasgupta005@gmail.com>
2021-11-25 22:23:51 +0530

    Add Support for MatchPage.js and add mapping in Backend.

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Switch} from 'react-router-dom'
 import { TeamPage } from './page/TeamPage';
+import { MatchPage } from './page/MatchPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
           <Routes>
-            <Route path = "/teams/:teamName" element = {<TeamPage />} />
+              <Route path = "teams/:teamName/matches/:year" element = {<MatchPage />} />
+              <Route path = "/teams/:teamName" element = {<TeamPage />} />
           </Routes>
       </Router>  
     </div>
